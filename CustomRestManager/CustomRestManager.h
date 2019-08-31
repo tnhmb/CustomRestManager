f3bbfd5755a1d60c18aa00448165da82cdbd0818
Tariq Bashuaib <t.tantona@gmail.com>
2019-08-31 10:12:29 +0800

    Initial Commit

---
+++ CustomRestManager/CustomRestManager.h
@@ -0,0 +1,19 @@
+//
+//  CustomRestManager.h
+//  CustomRestManager
+//
+//  Created by Tariq Najib on 31/08/2019.
+//  Copyright © 2019 Tariq Najib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomRestManager.
+FOUNDATION_EXPORT double CustomRestManagerVersionNumber;
+
+//! Project version string for CustomRestManager.
+FOUNDATION_EXPORT const unsigned char CustomRestManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRestManager/PublicHeader.h>
+
+
